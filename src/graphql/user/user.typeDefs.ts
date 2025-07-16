0b7e5c2193fe3ba0b5e9c55d43bd010721806fc0
liuqiba <85908203+liuqibasixseveneight@users.noreply.github.com>
2025-07-16 01:18:01 +0100

    chore: Documentation for type defs (#3)

--- src/graphql/user/user.typeDefs.ts
+++ src/graphql/user/user.typeDefs.ts
@@ -1,18 +1,39 @@
 import { gql } from 'apollo-server';
 
 export const userTypeDefs = gql`
+  """
+  Represents a user of the QRFolio platform
+  """
   type User {
+    """
+    Unique identifier (UUID) of the user
+    """
     id: ID!
+    """
+    Unique email address of the user
+    """
     email: String!
+    """
+    Optional display name of the user
+    """
     name: String
+    """
+    Timestamp for when the user account was created
+    """
     createdAt: String!
   }
 
   type Query {
+    """
+    Retrieves a list of all users
+    """
     users: [User!]!
   }
 
   type Mutation {
+    """
+    Creates a new user with an email address and name (email is required)
+    """
     createUser(email: String!, name: String): User!
   }
 `;
