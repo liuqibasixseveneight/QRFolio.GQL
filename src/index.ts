47a41866db8acd5bc62ef499473868d6c6e7c19f
liuqiba <85908203+liuqibasixseveneight@users.noreply.github.com>
2025-10-24 10:06:23 +0100

    feat: Hosting configuration (#13)

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,24 @@ import { ApolloServer } from 'apollo-server';
 import chalk from 'chalk';
 
 import { schema } from './graphql/schema';
-import { SERVER_PORT } from './config';
+import { SERVER_PORT, UI_PRODUCTION_URL } from './config';
 
 const server = new ApolloServer({
   typeDefs: schema.typeDefs,
   resolvers: schema.resolvers,
   cors: {
-    origin: '*',
+    origin: [
+      UI_PRODUCTION_URL,
+      'http://localhost:3000', // For local development
+      'http://localhost:3001', // Alternative local port
+    ],
     credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Apollo-Require-Preflight',
+    ],
   },
 });
 
